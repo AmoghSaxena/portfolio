2ee9258030b1bec0017cad70c18b91c2a905997c
AmoghSaxena <13078900+AmoghSaxena@users.noreply.github.com>
2024-09-05 12:28:33 +0100

    Updated Experiance

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -8,10 +8,10 @@ import { blue, green, pink, yellow } from "../utils";
 export const experiences = [
   {
     logo: stealth,
-    name: "Stealth Startup",
-    joined: "Sept'22",
-    end: "Feb'23",
-    title: "Full Stack Developer",
+    name: "Digivalet",
+    joined: "March'21",
+    end: "March'23",
+    title: "Software Developer",
     bio: "Utilized ExpressJS for backend authentication and rate limitation. The frontend ReactJS code was optimized, eliminating re-renders to enhance performance and user experience. A ReactJS and ExpressJS-based admin panel was developed for data visualization and transaction management.",
     color: blue,
   },
