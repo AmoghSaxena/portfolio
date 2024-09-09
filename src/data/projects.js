289f3908ca6af9e39b977568c6910b8427bf4043
AmoghSaxena <13078900+AmoghSaxena@users.noreply.github.com>
2024-09-09 01:38:58 +0100

    Test Project

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,63 +20,63 @@ export const projects = [
       "This project compares WGAN and VAE for image generation using the CIFAR-10 dataset.",
   },
   {
-    title: "Send form to me",
+    title: "Ubuntu-Shortcut-Creator",
     image: sendfrom,
-    github: "https://sendformto.me",
+    github: "https://github.com/AmoghSaxena/Ubuntu-Shortcut-Creator",
     description:
-      "Backend-as-a-service for creating forms with unique URLs and email notifications.",
+      "This script can help you to create a short in Ubuntu or any ubuntu based OS.",
   },
   {
-    title: "Fan Merch",
+    title: "PyPad",
     image:
       "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/fanmerch.png",
-    github: "https://github.com/teshank2137/fanMerch",
+    github: "https://github.com/AmoghSaxena/PyPad",
     // link: "https://fan-merch.vercel.app/",
     description:
-      "A conceptual website for social media influence's to sell their merch.",
+      "PyPad is a lightweight and easy-to-use text editor built with Python and the Tkinter library.",
   },
   {
-    title: "Find Techie",
+    title: "OnScreen Ticker & Promotions",
     description:
-      "A Twitter bot that scans Twitter for job/intern posting and retweets top tweets daily.",
+      "A Linux Program for On-Screen Advertisements and promotions like shown in News Channels.",
     image:
       "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/Techie.png",
-      github: "https://twitter.com/FindTechie",
+      github: "https://github.com/AmoghSaxena/OnScreen-Ticker-and-Promotions",
   },
   {
-    title: "React Infinite scroll",
-    description: "Infinite scrolling using custom hook and ObservableAPI",
+    title: "IPTV Stream Verification",
+    description: "The program is simple and used to check the working of IPTV Channels.",
     image: scroll,
-    link: "https://react-infinite-scroll-nu.vercel.app/",
-    github: "https://github.com/teshank2137/react-infinite-scroll",
+    // link: "https://react-infinite-scroll-nu.vercel.app/",
+    github: "https://github.com/AmoghSaxena/IPTV-Stream-Verification",
   },
-  {
-    title: "Smart Attendance App",
-    description:
-      "A mobile app in which the user can track and mark attendance using face verification.",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/smartattendance.png",
-    github: "https://github.com/teshank2137/smartAtendence",
-  },
-  {
-    title: "Stock Market Prediction API",
-    description:
-      "An API Which predicts the next day's closing price of a stock.",
-    image: stonks,
-    github: "https://github.com/teshank2137/stockprediction",
-  },
-  {
-    title: "Algorithm Visualizer",
-    description: "Web App to visualize the searching and sorting algorithms",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/sort.JPG",
-    link: "https://teshank2137.github.io/#/",
-  },
-  {
-    title: "Portfolio v1",
-    description: "Built using ReactJS, ThreeJS, CSS3 and lots of ❤️ love.",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/port.JPG",
-    link: "https://www.v1.teshank.dev/",
-  },
+  // {
+  //   title: "Smart Attendance App",
+  //   description:
+  //     "A mobile app in which the user can track and mark attendance using face verification.",
+  //   image:
+  //     "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/smartattendance.png",
+  //   github: "https://github.com/teshank2137/smartAtendence",
+  // },
+  // {
+  //   title: "Stock Market Prediction API",
+  //   description:
+  //     "An API Which predicts the next day's closing price of a stock.",
+  //   image: stonks,
+  //   github: "https://github.com/teshank2137/stockprediction",
+  // },
+  // {
+  //   title: "Algorithm Visualizer",
+  //   description: "Web App to visualize the searching and sorting algorithms",
+  //   image:
+  //     "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/sort.JPG",
+  //   link: "https://teshank2137.github.io/#/",
+  // },
+  // {
+  //   title: "Portfolio v1",
+  //   description: "Built using ReactJS, ThreeJS, CSS3 and lots of ❤️ love.",
+  //   image:
+  //     "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/port.JPG",
+  //   link: "https://www.v1.teshank.dev/",
+  // },
 ];
